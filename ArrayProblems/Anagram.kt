733dca55510a2beae1720d42b9dc1b2ca644e03d
Abhay Kaushal <abhay880244@gmail.com>
2024-02-28 12:28:51 +0530

    anagram added

---
+++ ArrayProblems/Anagram.kt
@@ -0,0 +1,45 @@
+package ArrayProblems
+
+
+fun main() {
+    println(isAnagram("a","a"))
+}
+
+fun isAnagram(s1: String, s2: String): Boolean {
+    // code goes here
+
+    if(s1.length != s2.length){
+        return false
+    }
+
+    val hashMap = HashMap<Char, Int>()
+
+    for(ch in s1.toCharArray()){
+        if(hashMap.containsKey(ch)){
+            hashMap.put(ch,(hashMap.get(ch)?:0)+1)
+        }
+        else {
+            hashMap.put(ch, 1)
+        }
+    }
+
+
+    for(ch in s2.toCharArray()){
+        if(hashMap.containsKey(ch)){
+            hashMap.put(ch,(hashMap.get(ch)?:0) - 1)
+        }
+        else {
+            hashMap.put(ch, 1)
+        }
+    }
+
+
+    for(v in hashMap.entries.toList()){
+        if(v.value != 0){
+            return false
+        }
+    }
+
+    return true
+
+}
